7fe197fda5b646a1b1ca4cbbfdc3c62086ad27d0
Kim Kwonil <rnjsdlf0322@gmail.com>
2023-04-10 15:59:24 +0900

    controller 및 service 생성

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BoardsController } from './boards.controller';
+import { BoardsService } from './boards.service';
 
-@Module({})
-export class BoardsModule {}
+@Module({
+    controllers:[BoardsController],
+    providers: [BoardsService]
+})
+export class BoardsModule {
+
+}
