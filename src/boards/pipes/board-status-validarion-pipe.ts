2a95e254a5beb00eb63d07855ce412aa143b0d9a
Kim Kwonil <rnjsdlf0322@gmail.com>
2023-04-13 14:51:41 +0900

    docker 추가

--- src/boards/pipes/board-status-validarion-pipe.ts
+++ src/boards/pipes/board-status-validarion-pipe.ts
@@ -1,9 +1,26 @@
-import { ArgumentMetadata, PipeTransform } from "@nestjs/common";
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { BoardStatus } from "../boards.model";
 
 export class BoardStatusValidationPipe implements PipeTransform {
-    transform(value: any, metadata: ArgumentMetadata) {
- 
+    readonly StatusOption = [
+        BoardStatus.PRIVATE,
+        BoardStatus.PUBLIC
+    ];
+
+    private isStatusValid(status: any) {
+        const index = this.StatusOption.indexOf(status);
+        return index != -1;
+    }
+
+    transform(value: any) {
+        value = value.toUpperCase();
+
+        if(!this.isStatusValid(value)) {
+            throw new BadRequestException(`${value} isn't in the status option`)
+        }
 
         return value;
     }
+
+
 }
---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,12 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+    type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'nestjs',
+      database: 'nestjsDB',
+      entities: [__dirname + '/../**/*.entity.{js,ts}'],
+      synchronize: true,
+} 
